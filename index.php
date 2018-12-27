819d8bfe5bb42007faafb7466fbbe9d3f74fa3b9
camdlp <carlosabiamer@gmail.com>
2018-12-27 17:38:47 +0100

    Terminado sideBar, implementado primer footer (no definitivo).

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ Menu: #64DD17  light-green accent-4
     <body>
 
         <!-- NAVBAR -->
-        <nav class="navbar-material">
+        <nav class="navbar-material red accent-2">
             <div class="nav-wrapper">
                 <a href="index.php" class="brand-logo center"><img class="responsive-img" id="logo" src="img/icon-color.png"/></a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
@@ -37,6 +37,16 @@ Menu: #64DD17  light-green accent-4
         </nav>
 
         <ul class="sidenav" id="mobile-demo">
+            <li>
+                <div class="user-view">
+                    <div class="background">
+                        <img src="img/icon-color.png" class="responsive-img" >
+                    </div>
+                    <a href="#user"><img class="circle white" src="img/icon-color.png" class="responsive-img"></a>
+                    <a href="#name"><span class="grey-text" id="nickSideBar">Acceder</span></a>
+                    <a href="#email"><span class="grey-text" id="emailSideBar">Registrarse</span></a>
+                </div>
+            </li>          
             <li><a href=""><i class="material-icons">account_circle</i>Cuenta</a></li>
             <li><a href=""><i class="material-icons">restaurant_menu</i>Menú</a></li>
             <li><a href=""><i class="material-icons">shopping_cart</i>Cesta</a></li>
@@ -44,6 +54,24 @@ Menu: #64DD17  light-green accent-4
         <!-- NAVBAR -->
 
 
+        <!-- CONTENIDO -->
+        <div id="contenido">
+            <blockquote>
+                This is an example quotation that uses the blockquote tag.
+            </blockquote>
+        </div>
+        <!-- CONTENIDO -->
+
+        <!-- FOOTER -->
+        <footer class="page-footer red accent-2">
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2019 Copyright Fast n' Healthy
+                    <a class="grey-text text-lighten-4 right" href="#!">Sobre nosotros</a>
+                </div>
+            </div>
+        </footer>
+        <!-- FOOTER -->
         <?php
         // put your code here
         ?>
